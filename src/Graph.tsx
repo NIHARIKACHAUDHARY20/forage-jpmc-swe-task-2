e5fe4ba322ec282196ff9afa7599b9dd0fadd7b0
NIHARIKACHAUDHARY20 <124075156+NIHARIKACHAUDHARY20@users.noreply.github.com>
2024-07-13 21:38:25 +0530

    Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -34,6 +34,10 @@ class Graph extends Component<IProps, {}> {
     // Get element to attach the table from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
     elem.setAttribute('view','y-axis');
+    elem.setAttribute('coloumn-pivots','["stocks"]');
+    elem.setAttribute('row-pivots','["timestamps"]');
+     elem.setAttribute('coloumn','["top_ask_price"]');
+     elem.setAttribute('aggregates','{"stocks":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}');
 
     const schema = {
       stock: 'string',
